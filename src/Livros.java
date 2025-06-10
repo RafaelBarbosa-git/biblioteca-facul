0467baf32448b293579c107b8bd6cd03fe1e2cbd
joseca <91603735+vjasu@users.noreply.github.com>
2025-06-10 08:54:50 -0300

    Livros.java

--- src/Livros.java
+++ src/Livros.java
@@ -1,19 +1,26 @@
-public class Livros extends Material{
-    private int genero;
+package projeto_2fase;
 
-    public Livros(String titulo, String autor, int genero) {
-        super(titulo, autor);
-        this.genero = genero;
-    }
+public class Livros extends Material {
+	private int genero;
 
-    public int getGenero() {
-        return genero;
-    }
+	public Livros(String titulo, String autor, int genero) {
+		super(titulo, autor);
+		this.genero = genero;
 
-    @Override
-    public String toString() {
-        return "Livros:\n- Titulo = " + super.getTitulo() +
-                "\n- Autor: " + super.getAutor() +
-                "\n- Genero = " + this.genero;
-    }
+	}
+
+	public int getGenero() {
+		return genero;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	@Override
+	public String toString() {
+		return "Livros:\n- Titulo = " + super.getTitulo() + "\n- Autor: " + super.getAutor() + "\n- Genero = "
+				+ this.genero;
+	}
+
 }
